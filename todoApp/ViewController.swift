ba72f29a186ac47b0a76f63e79261ea992d0fe17
Jason Chow <jasonchoww@gmail.com>
2018-10-20 20:20:53 -0700

    Update, added task text field

--- todoApp/ViewController.swift
+++ todoApp/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ToDoViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var list = [String]()
-    var testList = ["test", "test", "homework"]
+    var list = [""]
+    var testList = ["test"]
     
+    @IBOutlet weak var textTaskField: UITextField!
+    @IBOutlet weak var taskTableView: UITableView!
+    
+    var textTaskFieldConverter: String{
+        if let text = textTaskField.text{
+            return text
+        }else{
+            return ""
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(testList.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        //cell.textLabel?.text = testList[indexPath.row]
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+    
+    
+    @IBAction func addTask(_ sender: Any) {
+        list.append(textTaskFieldConverter)
+        taskTableView.reloadData()
+        print(textTaskFieldConverter)
+    }
+    
+   
+    
+
     
 
     override func viewDidLoad() {
