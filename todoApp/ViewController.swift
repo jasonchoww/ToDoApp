05040632fe75a60f50107e83c521e87015cae320
Jason Chow <jasonchoww@gmail.com>
2018-10-21 18:42:39 -0700

    Update, added functionality to delete a task

--- todoApp/ViewController.swift
+++ todoApp/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var list = [""]
-    var testList = ["test"]
+    var list = ["corn", "milk", "eggs"]
     
     @IBOutlet weak var textTaskField: UITextField!
     @IBOutlet weak var taskTableView: UITableView!
+    
     
     var textTaskFieldConverter: String{
         if let text = textTaskField.text{
@@ -26,22 +26,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return(testList.count)
+        return(list.count)
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        //cell.textLabel?.text = testList[indexPath.row]
         cell.textLabel?.text = list[indexPath.row]
         return(cell)
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            self.list.remove(at: indexPath.row)
+            taskTableView.reloadData()
+        }
+    }
     
     
     @IBAction func addTask(_ sender: Any) {
+        if (textTaskFieldConverter != ""){
         list.append(textTaskFieldConverter)
+        }
+        textTaskField.text = ""
         taskTableView.reloadData()
-        print(textTaskFieldConverter)
     }
     
    
